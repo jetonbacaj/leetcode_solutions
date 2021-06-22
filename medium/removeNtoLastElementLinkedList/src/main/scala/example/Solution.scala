b84e1ac565e035cbe0d2a515008dd0d822a58a80
Jeton Bacaj <jeton>
2021-06-22 03:28:02 -0400

    Finished & it is passing the submission tests in leetcode

--- medium/removeNtoLastElementLinkedList/src/main/scala/example/Solution.scala
+++ medium/removeNtoLastElementLinkedList/src/main/scala/example/Solution.scala
@@ -1,10 +1,25 @@
 object Solution {
 
   def main(args: Array[String]): Unit = {
-    val res = searchRange(Array(0,0,1,1,2,2,2,2,3,3,4,4,4,5,6,6,6,7,8,8),8)
 
-    println(res.toList)
+    val foo = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5)))))
+
+    print("before: [")
+    printLinkedList(foo)
+
+    val bar = removeNthFromEnd(foo,6)
+
+    print("after: [")
+    printLinkedList(bar)
   }  
+
+  def printLinkedList(head: ListNode){
+    if(head == null) println("]")
+    else {
+      print(s"${head.x},")
+      printLinkedList(head.next)
+    }
+  }
 
   def removeNthFromEnd(head: ListNode, n: Int): ListNode = {
     /*
@@ -15,7 +30,41 @@ object Solution {
     })
     */
 
-    ???
+    @annotation.tailrec
+    def countIter(workingHead: ListNode, count: Int): Int = {
+      workingHead match {
+        case null => count
+        case _ => countIter(workingHead.next, count + 1)
+      }
+    }
+
+    @annotation.tailrec
+    def iter(last: Option[ListNode], curr: ListNode, counter: Int): ListNode = {
+
+      (counter compareTo 0) match {
+
+        //(1,2,3,4,5), 2 => 4 ==> (1,2,3,5)
+
+        case 0 => 
+          last.map(l => l.next = curr.next)
+          head
+
+        case 1 => iter(Some(curr), curr.next, counter - 1)
+
+        case _ => throw new RuntimeException("This should not happen!")
+      }
+
+    }
+
+    val elementCount = countIter(head, 0)
+    val difference = elementCount - n
+
+    //if(difference == 0) head
+    //else 
+    if(difference < 0) null
+    else if (difference == 0) head.next
+    else if(elementCount == n) head
+    else iter(None, head, difference)  
   }
 
   class ListNode(_x: Int = 0, _next: ListNode = null) {
