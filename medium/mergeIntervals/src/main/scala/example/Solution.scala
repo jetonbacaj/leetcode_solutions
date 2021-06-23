4c6f44aefd6535700a3d1b6c8cfd798d22f9c91a
Jeton Bacaj <jeton>
2021-06-23 00:49:40 -0400

    Added interval solutions

---
+++ medium/mergeIntervals/src/main/scala/example/Solution.scala
@@ -0,0 +1,33 @@
+
+object Solution {
+
+  def merge(intervals: Array[Array[Int]]): Array[Array[Int]] = {
+      
+      // assuming the pairs are ordered!!!!
+      val pairs = intervals.map(x=> (x(0), x(1))).toList.sortBy(_._1)
+      
+      @annotation.tailrec
+      def iter(acc: List[(Int, Int)], intervals: List[(Int, Int)], tupleOpt: Option[(Int, Int)]): List[(Int, Int)] = {
+
+        (tupleOpt, intervals) match {
+                    
+          case (Some(tuple), h::t) =>
+            if((tuple._1 <= h._1 && tuple._2 >= h._1 && tuple._2 <= h._2) ||
+               (tuple._1 <= h._1 && tuple._2 >= h._1 && tuple._2 >= h._2))
+              iter(acc, t, Some((tuple._1, math.max(tuple._2, h._2))))
+            else iter(tuple :: acc, t, Some(h))
+          
+          case (None, h::t) =>
+            iter(acc, t, Some(h))
+          
+          case (Some(tuple), Nil) =>
+            tuple :: acc
+          
+          case (None, Nil) =>
+            acc
+        }
+      }
+      
+      iter(List.empty, pairs, None).map(x => Array(x._1, x._2)).reverse.toArray
+    }
+}
